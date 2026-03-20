8acbe4ec2d7ec9264d31e31b2348935bbc87408d
Querdo <querdo7@gmail.com>
2026-03-21 00:57:37 +0400

    Update 1.0 - Был исправлен models.py, был добавлен main.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, r
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 engine = create_engine("sqlite:///store.db", echo=True)
@@ -10,9 +10,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 class Users (Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    password = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(20), unique=True)
+    password = Column(String)
     balance = Column(Integer, default=5000)
 
     cart_items = relationship("Cart", back_populates="user")
@@ -20,13 +20,13 @@ class Users (Base):
 class Game (Base):
     __tablename__ = "games"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(20))
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
     price = Column(Integer)
-    type = Column(String(20))
+    genre = Column(String(20))
     author = Column(String(20))
-    description = Column(String(20))
-    image_url = Column(String(100))
+    description = Column(String(255))
+    image_url = Column(String(255))
 
     cart_items = relationship("Cart", back_populates="game")
 
@@ -34,10 +34,11 @@ class Game (Base):
 class Cart (Base):
     __tablename__ = "carts"
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False, )
-    game_id = Column(Integer, nullable=False)
-    quanity = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    game_id = Column(Integer, ForeignKey("games.id"))
+    quantity = Column(Integer, default=1)
+    status = Column(String, default="cart")
 
     user = relationship("User", back_populates="cart_items")
     game = relationship("Game", back_populates="cart_items")
